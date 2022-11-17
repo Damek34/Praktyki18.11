cc7848b1f99cb920678f83509be4a3356f541dd5
Damian Cichosz <damekjodamek@gmail.com>
2022-11-17 15:45:33 +0100

    zad2

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const numbers = [1, 2, 3, 4, 5];
 
 const polacz = (array, array2) =>  [...array, ...array2];
 
+var ostatni = array => 
+{
+    array.reverse();
+    return array[0];
+}
+
+
 console.log(polacz(arr1, arr2));
+console.log(ostatni(arr2));
