ad600d9312ca1dbeb5253d738ffec3f7a6ddca4c
Damian Cichosz <damekjodamek@gmail.com>
2022-11-17 16:32:24 +0100

    1 zadanie dodatkowe

--- script.js
+++ script.js
@@ -12,15 +12,20 @@ var ostatni = array =>
 }
 
 
-var filtr = (array) => array.filter(array => array.length > 4);
+var filtr = array => array.filter(array => array.length > 4);
 
 
 var redukcja = numbers.reduce((previousValue, currentValue) => previousValue + currentValue);
 
 var tabMap = [...numbers].map(x => x*x+3);
 
+
+
+var dodatkowe1 = array => array.filter(arrayElement => arrayElement.length == 5 && arrayElement.endsWith("ek"));
+
 console.log(polacz(arr1, arr2));
 console.log(ostatni(arr2));
 console.log(filtr(arr2));
 console.log(redukcja);
 console.log(tabMap);
+console.log(dodatkowe1(arr2));
